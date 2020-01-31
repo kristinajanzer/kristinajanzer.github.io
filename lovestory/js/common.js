755414d23ad184227f21f24caa8f79a3d5ea2cea
kristinajanzer <60479956+kristinajanzer@users.noreply.github.com>
2020-01-31 16:02:57 +0300

    Add files via upload

---
+++ lovestory/js/common.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    $('.drop-menu').click(function () {
+        $(this).attr('tabindex', 1).focus();
+        $(this).toggleClass('active');
+        $(this).find('.dropeddown').slideToggle(300);
+    });
+    $('.drop-menu').focusout(function () {
+        $(this).removeClass('active');
+        $(this).find('.dropeddown').slideUp(300);
+    });
+    $('.drop-menu .dropeddown li').click(function () {
+        $(this).parents('.drop-menu').find('span').text($(this).text());
+        $(this).parents('.drop-menu').find('input').attr('value', $(this).attr('id'));
+    });
+
+});
